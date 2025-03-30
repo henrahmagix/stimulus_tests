5c14f320fe8104290ea9b55468d7614d25acf4f2
Henry Blyth <blyth.henry@gmail.com>
2025-03-30 21:24:25 +0100

    Allow gem to be required in other envs
    It just doesn't add the routes.

--- lib/stimulus_tests/railtie.rb
+++ lib/stimulus_tests/railtie.rb
@@ -1,16 +1,14 @@
 module StimulusTests
   class Railtie < ::Rails::Railtie
-    if Rails.env.test?
-      config.stimulus_tests = ActiveSupport::OrderedOptions.new
-      config.stimulus_tests.route_path = "/_stimulus_tests"
+    config.stimulus_tests = ActiveSupport::OrderedOptions.new
+    config.stimulus_tests.route_path = "/_stimulus_tests"
 
-      config.after_initialize do |app|
+    config.after_initialize do |app|
+      if Rails.env.test?
         route_path = config.stimulus_tests.route_path
         app.routes.prepend { get route_path => "stimulus_tests/visit#index" }
-      end
-    else
-      def config.stimulus_tests=(*)
-        raise NotImplementedError, "StimulusTests configuration is only available in test environment."
+      else
+        warn "StimulusTests is only available in the test environment."
       end
     end
   end
